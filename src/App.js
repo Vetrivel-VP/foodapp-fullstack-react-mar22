b097c1707ff95883415f0baa27b24ac745edd562
VETRIVEL <vetrivel.galaxy@gmail.com>
2022-03-16 18:53:31 +0300

    Changes : UI Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Header from "./components/Header";
 import BG from "./img/bg.png";
+import MobileBg from "./img/mobileBg.png";
 
 const App = () => {
   return (
     <div className="w-screen h-screen flex items-center justify-center relative overflow-hidden">
-      <img src={BG} className="min-w-full min-h-full object-cover" alt="" />
+      <img
+        src={BG}
+        className="hidden md:block w-full h-full object-cover"
+        alt=""
+      />
+      <img
+        src={MobileBg}
+        className="block md:hidden w-full h-full object-cover"
+        alt=""
+      />
       <main className="absolute inset-0">
         <Header />
       </main>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Logo from "../img/logo.png";
+import Avatar from "../img/avatar.png";
 import { NavLink } from "react-router-dom";
+import { MdShoppingCart } from "react-icons/md";
 
 const isActiveStyles =
   "text-green-700 font-semibold text-xl font-normal relative before:rounded-lg before:animate-pulse  before:absolute before:content before:w-5 before:h-1 before:bottom-0 before:left-4 before:bg-green-700 transition-all ease-in-out duration-100";
@@ -11,25 +13,38 @@ const isNotActiveStyles =
 const Header = () => {
   return (
     <div className="w-full px-8 py-4 flex items-center justify-center  ">
-      <NavLink
-        to={"/menu"}
-        className={({ isActive }) =>
-          isActive ? isActiveStyles : isNotActiveStyles
-        }
-      >
-        Menu
-      </NavLink>
-      <NavLink to={"/"}>
-        <img src={Logo} className="w-40 mx-16" alt="" />
-      </NavLink>
-      <NavLink
-        to={"/contact"}
-        className={({ isActive }) =>
-          isActive ? isActiveStyles : isNotActiveStyles
-        }
-      >
-        Contact
-      </NavLink>
+      <img
+        src={Avatar}
+        className="w-14 h-14 cursor-pointer rounded-full shadow-xl mr-auto"
+        alt=""
+      />
+
+      <div className="flex items-center">
+        <NavLink
+          to={"/menu"}
+          className={({ isActive }) =>
+            isActive ? isActiveStyles : isNotActiveStyles
+          }
+        >
+          Menu
+        </NavLink>
+        <NavLink to={"/"}>
+          <img src={Logo} className="w-40 mx-16" alt="" />
+        </NavLink>
+        <NavLink
+          to={"/contact"}
+          className={({ isActive }) =>
+            isActive ? isActiveStyles : isNotActiveStyles
+          }
+        >
+          Contact
+        </NavLink>
+      </div>
+
+      <div className="flex items-center gap-2 bg-black p-2 rounded-md ml-auto">
+        <MdShoppingCart className="text-xl text-white" />
+        <p className="text-base text-white font-semibold">My Cart</p>
+      </div>
     </div>
   );
 };
