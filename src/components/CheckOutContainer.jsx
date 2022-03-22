3b8c54196a8ad9c6cae6bdd53e4a36fc5343f999
VETRIVEL <vetrivel.galaxy@gmail.com>
2022-03-22 19:05:24 +0300

    Changes: Ui Updated

--- src/components/CheckOutContainer.jsx
+++ src/components/CheckOutContainer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CheckOutContainer = () => {
   return (
     <div className="w-full md:w-375 h-screen bg-white fixed top-0 right-0 drop-shadow-md">
-      CheckOutContainer
+      CheckOutContainer some
     </div>
   );
 };
